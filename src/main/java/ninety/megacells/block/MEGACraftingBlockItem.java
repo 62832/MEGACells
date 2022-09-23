f1fe40d8b2bd2f02429d7f9c53d9e1f63164aa80
90 <90@national.shitposting.agency>
2022-09-23 18:25:39 +0100

    Add clarifying tooltip to MEGA co-processing unit

--- src/main/java/ninety/megacells/block/MEGACraftingBlockItem.java
+++ src/main/java/ninety/megacells/block/MEGACraftingBlockItem.java
@@ -1,11 +1,15 @@
 package ninety.megacells.block;
 
+import java.util.List;
 import java.util.function.Supplier;
 
+import appeng.core.localization.Tooltips;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -31,5 +35,12 @@ public class MEGACraftingBlockItem extends CraftingBlockItem {
             return InteractionResultHolder.sidedSuccess(player.getItemInHand(hand), level.isClientSide());
         }
         return super.use(level, player, hand);
+    }
+
+    @Override
+    public void addCheckedInformation(ItemStack itemStack, Level level, List<Component> toolTip, TooltipFlag advancedTooltips) {
+        if (this.getBlock().equals(MEGABlocks.CRAFTING_ACCELERATOR.asBlock())) {
+            toolTip.add(Tooltips.of("Provides 4 co-processing threads per block."));
+        }
     }
 }
