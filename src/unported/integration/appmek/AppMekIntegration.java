0de64a4009836b13a5a58828203a644f182fa059
netherportal3 <151792930+netherportal3@users.noreply.github.com>
2024-07-07 16:13:45 -0400

    Update AppMekIntegration.java
    Untested, no environment available for me to test.

--- src/unported/integration/appmek/AppMekIntegration.java
+++ src/unported/integration/appmek/AppMekIntegration.java
@@ -14,20 +14,20 @@ public final class AppMekIntegration {
     public static void initUpgrades() {
         var storageCellGroup = GuiText.StorageCells.getTranslationKey();
         var portableCellGroup = GuiText.PortableCells.getTranslationKey();
-
+    
         AppMekItems.getCells().forEach(cell -> {
             Upgrades.add(AEItems.INVERTER_CARD, cell, 1, storageCellGroup);
             Upgrades.add(AEItems.EQUAL_DISTRIBUTION_CARD, cell, 1, storageCellGroup);
             Upgrades.add(AEItems.VOID_CARD, cell, 1, storageCellGroup);
         });
-
+    
         AppMekItems.getPortables().forEach(portable -> {
             Upgrades.add(AEItems.INVERTER_CARD, portable, 1, portableCellGroup);
             Upgrades.add(MEGAItems.GREATER_ENERGY_CARD, portable, 2, portableCellGroup);
             Upgrades.add(AEItems.EQUAL_DISTRIBUTION_CARD, portable, 1, portableCellGroup);
             Upgrades.add(AEItems.VOID_CARD, portable, 1, portableCellGroup);
         });
-
+    
         for (var portable : List.of(
                 AMItems.PORTABLE_CHEMICAL_CELL_1K,
                 AMItems.PORTABLE_CHEMICAL_CELL_4K,
@@ -36,5 +36,12 @@ public final class AppMekIntegration {
                 AMItems.PORTABLE_CHEMICAL_CELL_256K)) {
             Upgrades.add(MEGAItems.GREATER_ENERGY_CARD, portable.get(), 2, portableCellGroup);
         }
+    
+        // Add new radioactive cells
+        AppMekItems.getRadioactiveCells().forEach(cell -> {
+            Upgrades.add(AEItems.INVERTER_CARD, cell.get(), 1, storageCellGroup);
+            Upgrades.add(AEItems.EQUAL_DISTRIBUTION_CARD, cell.get(), 1, storageCellGroup);
+            Upgrades.add(AEItems.VOID_CARD, cell.get(), 1, storageCellGroup);
+        });
     }
 }
