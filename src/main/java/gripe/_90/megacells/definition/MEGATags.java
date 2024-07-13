eaa294d6122352118d9df88b34317870d3bd8a52
90 <hi@90.gripe>
2024-07-13 20:48:34 +0100

    Switch to "c" namespace for material tags

--- src/main/java/gripe/_90/megacells/definition/MEGATags.java
+++ src/main/java/gripe/_90/megacells/definition/MEGATags.java
@@ -10,14 +10,14 @@ import gripe._90.megacells.MEGACells;
 
 public final class MEGATags {
     public static final TagKey<Item> SKY_STEEL_INGOT =
-            TagKey.create(Registries.ITEM, ResourceLocation.parse("forge:ingots/sky_steel"));
+            TagKey.create(Registries.ITEM, ResourceLocation.parse("c:ingots/sky_steel"));
     public static final TagKey<Item> SKY_BRONZE_INGOT =
-            TagKey.create(Registries.ITEM, ResourceLocation.parse("forge:ingots/sky_bronze"));
+            TagKey.create(Registries.ITEM, ResourceLocation.parse("c:ingots/sky_bronze"));
 
     public static final TagKey<Block> SKY_STEEL_BLOCK =
-            TagKey.create(Registries.BLOCK, ResourceLocation.parse("forge:storage_blocks/sky_steel"));
+            TagKey.create(Registries.BLOCK, ResourceLocation.parse("c:storage_blocks/sky_steel"));
     public static final TagKey<Block> SKY_BRONZE_BLOCK =
-            TagKey.create(Registries.BLOCK, ResourceLocation.parse("forge:storage_blocks/sky_bronze"));
+            TagKey.create(Registries.BLOCK, ResourceLocation.parse("c:storage_blocks/sky_bronze"));
 
     public static final TagKey<Item> MEGA_INTERFACE = TagKey.create(Registries.ITEM, MEGABlocks.MEGA_INTERFACE.id());
     public static final TagKey<Item> MEGA_PATTERN_PROVIDER =
