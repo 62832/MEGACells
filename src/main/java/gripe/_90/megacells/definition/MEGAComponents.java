d8ba8fec30fa5b470edfadab15841cba65970866
90 <hi@90.gripe>
2024-10-23 13:40:57 +0100

    Simplify component registration

--- src/main/java/gripe/_90/megacells/definition/MEGAComponents.java
+++ src/main/java/gripe/_90/megacells/definition/MEGAComponents.java
@@ -1,13 +1,13 @@
 package gripe._90.megacells.definition;
 
 import java.math.BigInteger;
-import java.util.function.Consumer;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -24,7 +24,7 @@ public final class MEGAComponents {
             DeferredRegister.create(Registries.DATA_COMPONENT_TYPE, MEGACells.MODID);
 
     public static final DataComponentType<AEKey> BULK_CELL_ITEM =
-            register("bulk_item", builder -> builder.persistent(AEKey.CODEC).networkSynchronized(AEKey.STREAM_CODEC));
+            register("bulk_item", AEKey.CODEC, AEKey.STREAM_CODEC);
 
     private static final Codec<BigInteger> BIG_INTEGER_CODEC = Codec.STRING.comapFlatMap(
             str -> {
@@ -47,28 +47,28 @@ public final class MEGAComponents {
             BigInteger::toString);
 
     public static final DataComponentType<BigInteger> BULK_CELL_UNIT_COUNT =
-            register("bulk_unit_count", builder -> builder.persistent(BIG_INTEGER_CODEC)
-                    .networkSynchronized(BIG_INTEGER_STREAM_CODEC));
+            register("bulk_unit_count", BIG_INTEGER_CODEC, BIG_INTEGER_STREAM_CODEC);
 
     public static final DataComponentType<BigInteger> BULK_CELL_UNIT_FACTOR =
-            register("bulk_unit_factor", builder -> builder.persistent(BIG_INTEGER_CODEC)
-                    .networkSynchronized(BIG_INTEGER_STREAM_CODEC));
+            register("bulk_unit_factor", BIG_INTEGER_CODEC, BIG_INTEGER_STREAM_CODEC);
 
-    public static final DataComponentType<DecompressionPattern.Encoded> ENCODED_DECOMPRESSION_PATTERN =
-            register("encoded_decompression_pattern", builder -> builder.persistent(DecompressionPattern.Encoded.CODEC)
-                    .networkSynchronized(DecompressionPattern.Encoded.STREAM_CODEC));
+    public static final DataComponentType<DecompressionPattern.Encoded> ENCODED_DECOMPRESSION_PATTERN = register(
+            "encoded_decompression_pattern",
+            DecompressionPattern.Encoded.CODEC,
+            DecompressionPattern.Encoded.STREAM_CODEC);
 
-    public static final DataComponentType<AEKey> RADIOACTIVE_CELL_CHEMICAL = register(
-            "radioactive_chemical", builder -> builder.persistent(AEKey.CODEC).networkSynchronized(AEKey.STREAM_CODEC));
+    public static final DataComponentType<AEKey> RADIOACTIVE_CELL_CHEMICAL =
+            register("radioactive_chemical", AEKey.CODEC, AEKey.STREAM_CODEC);
 
     public static final DataComponentType<Long> RADIOACTIVE_CELL_AMOUNT =
-            register("radioactive_amount", builder -> builder.persistent(Codec.LONG)
-                    .networkSynchronized(ByteBufCodecs.VAR_LONG));
+            register("radioactive_amount", Codec.LONG, ByteBufCodecs.VAR_LONG);
 
-    private static <T> DataComponentType<T> register(String name, Consumer<DataComponentType.Builder<T>> customizer) {
-        var builder = DataComponentType.<T>builder();
-        customizer.accept(builder);
-        var componentType = builder.build();
+    private static <T> DataComponentType<T> register(
+            String name, Codec<T> codec, StreamCodec<? super RegistryFriendlyByteBuf, T> streamCodec) {
+        var componentType = DataComponentType.<T>builder()
+                .persistent(codec)
+                .networkSynchronized(streamCodec)
+                .build();
         DR.register(name, () -> componentType);
         return componentType;
     }
