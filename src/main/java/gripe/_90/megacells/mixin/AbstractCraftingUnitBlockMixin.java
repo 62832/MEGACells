55be4fc89b8a134bf30cf9de6b88ae3baee4a4a3
90 <hi@90.gripe>
2024-12-19 15:52:32 +0000

    Account for the same upgrade item being used in different upgrade recipes Closes #156

--- src/main/java/gripe/_90/megacells/mixin/AbstractCraftingUnitBlockMixin.java
+++ src/main/java/gripe/_90/megacells/mixin/AbstractCraftingUnitBlockMixin.java
@@ -3,6 +3,7 @@ package gripe._90.megacells.mixin;
 import java.util.Objects;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,6 +15,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -22,6 +24,7 @@ import appeng.block.AEBaseBlock;
 import appeng.block.crafting.AbstractCraftingUnitBlock;
 import appeng.block.crafting.ICraftingUnitType;
 import appeng.core.definitions.AEBlocks;
+import appeng.recipes.AERecipeTypes;
 
 import gripe._90.megacells.MEGACells;
 import gripe._90.megacells.block.MEGACraftingUnitType;
@@ -45,6 +48,9 @@ public abstract class AbstractCraftingUnitBlockMixin extends AEBaseBlock {
 
     @Shadow
     public abstract InteractionResult removeUpgrade(Level level, Player player, BlockPos pos, BlockState newState);
+
+    @Shadow
+    protected abstract boolean transform(Level level, BlockPos pos, BlockState state);
 
     // spotless:off
     @Redirect(
@@ -71,22 +77,44 @@ public abstract class AbstractCraftingUnitBlockMixin extends AEBaseBlock {
     }
 
     // spotless:off
-    @ModifyExpressionValue(
+    @Redirect(
             method = "upgrade",
             at = @At(
                     value = "INVOKE",
                     target = "Lappeng/recipes/game/CraftingUnitTransformRecipe;getUpgradedBlock(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/world/level/block/Block;"))
     // spotless:on
-    private Block isMegaUpgrade(Block original) {
-        if (original == null) {
-            return null;
+    private Block isMegaUpgrade(Level level, ItemStack heldItem) {
+        for (var holder : level.getRecipeManager().getAllRecipesFor(AERecipeTypes.CRAFTING_UNIT_TRANSFORM)) {
+            if (heldItem.is(holder.value().getUpgradeItem())) {
+                var upgraded = holder.value().getUpgradedBlock();
+
+                if (BuiltInRegistries.BLOCK
+                        .getKey((AbstractCraftingUnitBlock<?>) (Object) this)
+                        .getNamespace()
+                        .equals(BuiltInRegistries.BLOCK.getKey(upgraded).getNamespace())) {
+                    return upgraded;
+                }
+            }
         }
 
-        return BuiltInRegistries.BLOCK.getKey(original).getNamespace().equals(MEGACells.MODID)
-                        && !Objects.requireNonNull(getRegistryName())
-                                .getNamespace()
-                                .equals(MEGACells.MODID)
-                ? null
+        return null;
+    }
+
+    // spotless:off
+    @ModifyExpressionValue(
+            method = "upgrade",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lappeng/block/crafting/AbstractCraftingUnitBlock;removeUpgrade(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Lnet/minecraft/world/InteractionResult;"))
+    // spotless:on
+    private InteractionResult upgradeMegaUnit(
+            InteractionResult original,
+            @Local(argsOnly = true) BlockState state,
+            @Local(argsOnly = true) Level level,
+            @Local(argsOnly = true) BlockPos pos,
+            @Local(name = "newState") BlockState newState) {
+        return state.getBlock() == MEGABlocks.MEGA_CRAFTING_UNIT.block()
+                ? transform(level, pos, newState) ? InteractionResult.SUCCESS : InteractionResult.FAIL
                 : original;
     }
 }
