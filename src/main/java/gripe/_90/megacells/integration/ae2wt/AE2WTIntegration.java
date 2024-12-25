17537044491778a862bdf696dd40c4ccd470868f
90 <hi@90.gripe>
2024-12-25 13:08:54 +0000

    Static imports are pointless

--- src/main/java/gripe/_90/megacells/integration/ae2wt/AE2WTIntegration.java
+++ src/main/java/gripe/_90/megacells/integration/ae2wt/AE2WTIntegration.java
@@ -1,14 +1,13 @@
 package gripe._90.megacells.integration.ae2wt;
 
-import static gripe._90.megacells.definition.MEGAItems.GREATER_ENERGY_CARD;
-
 import de.mari_023.ae2wtlib.api.registration.UpgradeHelper;
 
+import gripe._90.megacells.definition.MEGAItems;
 import gripe._90.megacells.integration.IntegrationHelper;
 
 public final class AE2WTIntegration implements IntegrationHelper {
     @Override
     public void initUpgrades() {
-        UpgradeHelper.addUpgradeToAllTerminals(GREATER_ENERGY_CARD, 0);
+        UpgradeHelper.addUpgradeToAllTerminals(MEGAItems.GREATER_ENERGY_CARD, 0);
     }
 }
