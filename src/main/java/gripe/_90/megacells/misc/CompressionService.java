d9456c2cfa96f7361fc273f8f5e1ce7433cc8d89
90 <hi@90.gripe>
2025-04-22 22:02:21 +0100

    Rework chain generation to actively remove recipe candidates already used for a previous chain

--- src/main/java/gripe/_90/megacells/misc/CompressionService.java
+++ src/main/java/gripe/_90/megacells/misc/CompressionService.java
@@ -3,16 +3,13 @@ package gripe._90.megacells.misc;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.world.item.Item;
@@ -35,14 +32,21 @@ public class CompressionService {
     private static final Logger LOGGER = LoggerFactory.getLogger(CompressionService.class);
     private static final CompressionChain EMPTY = new CompressionChain(List.of());
 
-    // Each chain is a list of "variants", where each variant consists of the item itself along with an associated value
-    // dictating how much of the previous variant's item is needed to compress into that variant.
-    // This value will be between 1 and 9 (inclusive) for any given item, or just 1 for the smallest base variant.
+    /**
+     * Each chain is a list of "variants", where each variant consists of the item itself along with an associated value
+     * dictating how much of the previous variant's item is needed to compress into that variant. This value will be
+     * between 1 and 9 (inclusive) for any given item, or just 1 for the smallest base variant.
+     */
     private static final List<CompressionChain> chains = new ArrayList<>();
-    private static final Map<Item, Integer> chainIndexCache = new HashMap<>();
 
-    // Should always be true on the client side, only the server needs to toggle this when loading
-    private static boolean loaded = true;
+    /**
+     * So as not to keep reiterating a potentially large list of chains to search for the chain containing a given item,
+     * this map is used to keep track of which items have already been used to request a chain and cache the index
+     * within the {@code chains} field at which that chain has been found. If a chain is present, this index is then
+     * tracked for every variant item within it, otherwise an index of {@code -1} is used to denote that a chain does
+     * not exist for the item.
+     */
+    private static final Map<Item, Integer> chainIndexes = new HashMap<>();
 
     public static void init() {
         GridServices.register(DecompressionService.class, DecompressionService.class);
@@ -64,12 +68,20 @@ public class CompressionService {
         });
     }
 
-    public static CompressionChain getChain(Item item) {
+    /**
+     * Retrieves a compression chain containing a given item as any of its "variants".
+     *
+     * @param item The item to retrieve a corresponding chain for.
+     * @return The {@link CompressionChain} corresponding to this item, or the {@code EMPTY} chain if no real chain
+     * exists for it or the given item was {@code null}.
+     */
+    @NotNull
+    public static CompressionChain getChain(@Nullable Item item) {
         if (item == null) {
             return EMPTY;
         }
 
-        var cachedIndex = chainIndexCache.get(item);
+        var cachedIndex = chainIndexes.get(item);
 
         if (cachedIndex != null) {
             return cachedIndex >= 0 ? chains.get(cachedIndex) : EMPTY;
@@ -79,30 +91,38 @@ public class CompressionService {
             var chain = chains.get(i);
 
             if (chain.containsVariant(item)) {
-                chainIndexCache.put(item, i);
+                for (var j = 0; j < chain.size(); j++) {
+                    chainIndexes.put(chain.getItem(j), i);
+                }
+
                 return chain;
             }
         }
 
-        if (loaded) {
-            chainIndexCache.put(item, -1);
-        }
-
+        chainIndexes.put(item, -1);
         return EMPTY;
     }
 
+    /**
+     * Synchronises chains gathered on the server to connected clients. Required in order for the compression
+     * <b>cutoff</b> feature of Bulk Cells to be accessible to clients playing on a multiplayer server.
+     */
     public static void syncToClient(SyncCompressionChainsPacket packet, IPayloadContext context) {
         context.enqueueWork(() -> {
             chains.clear();
-            chainIndexCache.clear();
+            chainIndexes.clear();
             chains.addAll(packet.chains());
         });
     }
 
+    /**
+     * (Re-)Initialises all compression chains upon initial server start-up, and upon any datapack {@code /reload}s in
+     * case of any crafting recipes changing which may differ from those used to generate previous chains.
+     */
     private static void loadRecipes(RecipeManager recipeManager, RegistryAccess access) {
         // Clear old chain cache in case of the server restarting or recipes being reloaded
-        loaded = false;
         chains.clear();
+        chainIndexes.clear();
 
         var compressed = new ArrayList<CraftingRecipe>();
         var decompressed = new ArrayList<CraftingRecipe>();
@@ -122,35 +142,40 @@ public class CompressionService {
         compressed.removeIf(recipe -> isIrreversible(recipe, decompressed, overrides, access));
         decompressed.removeIf(recipe -> isIrreversible(recipe, compressed, overrides, access));
 
+        // Prioritise recipes whose ingredients have only one potential item, to try and mitigate situations where items
+        // have not been unified properly and some (modded) item's subsequent variant is of an identical resource from
+        // a different mod than intended
         var ingredientSize = Comparator.<CraftingRecipe>comparingInt(
                 r -> r.getIngredients().getFirst().getItems().length);
         compressed.sort(ingredientSize);
         decompressed.sort(ingredientSize);
 
         // Pull all available compression chains from the recipe shortlist and add these to the cache
-        Stream.concat(compressed.stream(), decompressed.stream()).forEach(recipe -> {
-            var baseVariant = recipe.getResultItem(access).getItem();
-
-            if (getChain(baseVariant).isEmpty()) {
-                chains.add(generateChain(baseVariant, compressed, decompressed, overrides, access));
-            }
-        });
+        while (!compressed.isEmpty()) {
+            var baseVariant = compressed.removeFirst().getResultItem(access).getItem();
+            decompressed.removeIf(recipe -> recipe.getResultItem(access).is(baseVariant));
+            chains.add(generateChain(baseVariant, compressed, decompressed, overrides, access));
+        }
 
         LOGGER.info("Initialised bulk compression. {} compression chains gathered.", chains.size());
-        loaded = true;
-        chainIndexCache.clear();
     }
 
+    /**
+     * Generates and returns a new {@link CompressionChain} given an initial "base variant" item, two lists of
+     * "compression" and "decompression" recipe candidates and a list of "overrides" gathered previously. Operates
+     * destructively on the given recipe lists in order to remove corresponding "opposite" recipes and prevent further
+     * unnecessary reiterations with items already put into a previous chain.
+     */
     private static CompressionChain generateChain(
             Item baseVariant,
             List<CraftingRecipe> compressed,
             List<CraftingRecipe> decompressed,
             List<Override> overrides,
             RegistryAccess access) {
-        var variants = new LinkedList<Item>();
-        var multipliers = new LinkedList<Integer>();
+        var variants = new ArrayList<Item>();
+        var multipliers = new ArrayList<Integer>();
 
-        variants.addFirst(baseVariant);
+        variants.add(baseVariant);
 
         for (var lower = getNextVariant(baseVariant, decompressed, overrides, false, access); lower != null; ) {
             var item = lower.item();
@@ -165,16 +190,17 @@ public class CompressionService {
                 break;
             }
 
-            variants.addFirst(item);
-            multipliers.addFirst(lower.factor());
+            variants.add(item);
+            multipliers.add(lower.factor());
+            compressed.removeIf(recipe -> recipe.getResultItem(access).is(item));
             lower = getNextVariant(item, decompressed, overrides, false, access);
         }
 
-        multipliers.addFirst(1);
-        var chain = new ObjectArrayList<CompressionChain.Variant>();
+        multipliers.add(1);
+        var chain = new ArrayList<CompressionChain.Variant>();
 
-        for (var i = 0; i < variants.size(); i++) {
-            chain.add(new CompressionChain.Variant(variants.get(i), multipliers.get(i)));
+        for (var i = variants.size(); i > 0; i--) {
+            chain.add(new CompressionChain.Variant(variants.get(i - 1), multipliers.get(i - 1)));
         }
 
         for (var higher = getNextVariant(baseVariant, compressed, overrides, true, access); higher != null; ) {
@@ -189,6 +215,8 @@ public class CompressionService {
             }
 
             chain.add(higher);
+            var item = higher.item();
+            decompressed.removeIf(recipe -> recipe.getResultItem(access).is(item));
             higher = getNextVariant(higher.item(), compressed, overrides, true, access);
         }
 
@@ -196,6 +224,10 @@ public class CompressionService {
         return new CompressionChain(chain);
     }
 
+    /**
+     * Retrieves the "next" {@link CompressionChain.Variant} for a given item based on a given list of recipes and
+     * overrides and whether the recipes given are "compression" or "decompression" recipes.
+     */
     private static CompressionChain.Variant getNextVariant(
             Item item,
             List<CraftingRecipe> recipes,
@@ -204,10 +236,12 @@ public class CompressionService {
             RegistryAccess access) {
         for (var override : overrides) {
             if (compressed && override.smaller.equals(item)) {
+                overrides.remove(override);
                 return new CompressionChain.Variant(override.larger, override.factor);
             }
 
             if (!compressed && override.larger.equals(item)) {
+                overrides.remove(override);
                 return new CompressionChain.Variant(override.smaller, override.factor);
             }
         }
@@ -215,6 +249,7 @@ public class CompressionService {
         for (var recipe : recipes) {
             for (var input : recipe.getIngredients().getFirst().getItems()) {
                 if (input.getItem().equals(item)) {
+                    recipes.remove(recipe);
                     return new CompressionChain.Variant(
                             recipe.getResultItem(access).getItem(),
                             compressed
@@ -227,10 +262,20 @@ public class CompressionService {
         return null;
     }
 
+    /**
+     * Tests that a given recipe is a "decompression" recipe, i.e. a recipe with only a single ingredient presumably
+     * being split into some quantity of a "smaller" result item.
+     */
     private static boolean isDecompressionRecipe(CraftingRecipe recipe) {
         return recipe.getIngredients().stream().filter(i -> !i.isEmpty()).count() == 1;
     }
 
+    /**
+     * Tests that a given recipe is a "compression" recipe, i.e. a recipe where every non-empty ingredient is the same
+     * and corresponds to the same set of item values. It is assumed that at least one such ingredient is being crafted
+     * up into a "larger" item such as a storage block, of which no more than one (1) of this resulting item should be
+     * crafted.
+     */
     private static boolean isCompressionRecipe(CraftingRecipe recipe, RegistryAccess access) {
         if (recipe.getResultItem(access).getCount() != 1) {
             return false;
@@ -269,6 +314,13 @@ public class CompressionService {
         return true;
     }
 
+    /**
+     * Tests a given recipe against a list of corresponding candidate recipes in the opposite direction to check whether
+     * the initial recipe is (ir)reversible. In other words, given a compression recipe and a list of candidate
+     * <b>de</b>compression recipes (without accounting for overrides), this method returns {@code true} if there is no
+     * corresponding decompression recipe which can be carried out to "reverse" the result of the initial compression
+     * recipe and hence restore the original quantity of the smaller variant item previously compressed.
+     */
     private static boolean isIrreversible(
             CraftingRecipe recipe, List<CraftingRecipe> candidates, List<Override> overrides, RegistryAccess access) {
         if (overrideRecipe(recipe, overrides, access)) {
@@ -312,9 +364,11 @@ public class CompressionService {
         return true;
     }
 
-    // It may be desirable for some items to be included as variants in a chain in spite of any recipes involving those
-    // items not being reversible. Hence, we override any reversibility checks and generate a variant for such an item
-    // based on its usually irreversible recipe.
+    /**
+     * It may be desirable for some items to be included as variants in a chain in spite of any recipes involving those
+     * items not being reversible. Hence, we override any reversibility checks and generate a variant for such an item
+     * based on its usually irreversible recipe.
+     */
     private static boolean overrideRecipe(CraftingRecipe recipe, List<Override> overrides, RegistryAccess access) {
         var output = recipe.getResultItem(access);
 
@@ -348,6 +402,11 @@ public class CompressionService {
         return false;
     }
 
+    /**
+     * Conversely, it may be decided that some items should not be handled by the compression system at all and instead
+     * excluded from any chains. In this case, a value of {@code "NONE"} for a given item within the overrides data map
+     * (or equivalently, the "air" item that this value resolves to) specifies that this item should be excluded.
+     */
     private static boolean isBlacklisted(ItemStack stack) {
         return stack.getItemHolder().getData(MEGADataMaps.COMPRESSION_OVERRIDE) == Items.AIR;
     }
